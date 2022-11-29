d44eba55b785cefab52739c682dafd07eb7634bd
shilpi dhiman <shilpi.dhiman@gmail.com>
2022-11-29 23:27:57 +0530

    27th Feb 2011

--- src/main-component/App/App.js
+++ src/main-component/App/App.js
@@ -1,30 +1,27 @@
-import React from 'react';
-import {BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import Homepage from '../HomePage'
-import BlogPage from '../blogpage'
-import BlogDetailsPage from '../BlogDetailsPage'
-import Footer from '../../components/footer'
-import Scrollbar from '../../components/scrollbar'
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Homepage from "../HomePage";
+// import BlogPage from "../blogpage";
+// import BlogDetailsPage from '../BlogDetailsPage'
+import Footer from "../../components/footer";
+import Scrollbar from "../../components/scrollbar";
+import "./App.css";
 
-
-const App = () => { 
-
+const App = () => {
   return (
-    <div className="App">
-       <Router>
-          <Switch>
-            <Route exact path='/' component={Homepage} />
-            <Route path='/home' component={Homepage} />
-            <Route path='/Blog' component={BlogPage} />
-            <Route path='/Blog-details' component={BlogDetailsPage} />
-          </Switch>
-          <Footer/>
-          <Scrollbar/>
+    <div className='App'>
+      <Router>
+        <Switch>
+          <Route exact path='/' component={Homepage} />
+          <Route path='/home' component={Homepage} />
+          {/* <Route path='/Blog' component={BlogPage} /> */}
+          {/* <Route path='/Blog-details' component={BlogDetailsPage} /> */}
+        </Switch>
+        <Footer />
+        <Scrollbar />
       </Router>
-      
     </div>
   );
-}
+};
 
 export default App;
